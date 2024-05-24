20c7c403715059a506a0a301a4fde82bd8e1bc89
Lev <tinkuus56@gmail.com>
2024-05-24 20:27:28 +0300

    Added glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main(int argc, char* argv[]) {
-  std::cout << "Hello!" << std::endl;
+int main(void) {
+  GLFWwindow* win;
+
+  if (!glfwInit()) {
+    return -1;
+  }
+
+  win = glfwCreateWindow(640, 480, "Hello!", NULL, NULL);
+
+  if (!win) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(win);
+
+  while (!glfwWindowShouldClose(win)) {
+    // glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(win);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
